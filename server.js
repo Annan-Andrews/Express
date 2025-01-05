57934969e55f0f86af2dc87aa380f7e9bd5301f1
Annan-Andrews <annanandrews03@gmail.com>
2025-01-05 14:08:46 +0530

    MongoDB_Added

--- server.js
+++ server.js
@@ -1,24 +1,72 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+const Port = process.env.PORT
+const db_link = process.env.DB_LINK
+
+mongoose.connect(db_link)
+.then(res =>{
+  console.log("DB connect successfully")
+}).catch(err => {
+  console.log("DB connection failed")
+}) 
 
 const app = express()
 
+app.use(express.json())
+
+const TestSchema = new mongoose.Schema({
+  name : String
+})
+
+const Test = mongoose.model('test', TestSchema)
+
+app.use((req, res, next)=>{
+  console.log("Working")
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Response for GET request')
+  Test.find()
+  .then(Names =>{
+    res.json({Names})
+  })
+  .catch(err=>{
+    res.send("Something went wrong")
+  })
 })
 
 app.post('/', (req, res) => {
-  res.send('Response for POST request')
+  // console.log(req.body)
+  const name = req.body.name
+  Test.create({name: name})
+  res.send("Name Added")
 })
 
 app.put('/', (req, res) => {
   res.send('Response for PUT request')
 })
 
-app.delete('/', (req, res) => {
-  res.send('Response for DELETE request')
+app.delete('/:id', (req, res) => {
+  Test.findByIdAndDelete(req.params.id)
+  .then(data =>{
+    if(data){
+      res.json({data})
+      res.send("Deleted")
+    }
+    else{
+      res.json({"message": "Name does not exist"})
+    }
+  })
+  .catch(err =>{
+    console.log(err)
+    res.json({"message": "Something went wrong"})
+  })
 })
 
 
-app.listen(4004, ()=>{
-  console.log("Server Started")
+app.listen(Port, ()=>{
+  console.log(`Server Started on http://localhost:${Port}`)
 })
